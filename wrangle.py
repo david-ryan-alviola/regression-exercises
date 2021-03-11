6e2d375d208c000725d7c23adc07f4a2d791d694
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2021-03-11 16:29:37 -0600

    Updated after review

--- wrangle.py
+++ wrangle.py
@@ -17,10 +17,17 @@ def acquire_telco(cached=True):
 def clean_telco(telco_df):
     clean_df = telco_df.copy()
     
+    # Only want two year contracts
     two_year_customers = clean_df[clean_df.contract_type == "Two year"]
+    
+    # Only need these columns
     two_year_customers = two_year_customers[['customer_id', 'monthly_charges', 'tenure', 'total_charges']]
+    
+    # Replace empty total charges with 0 since need 1 month of tenure to incur charges
     two_year_customers.total_charges = two_year_customers.total_charges.replace(" ", np.nan)
-    two_year_customers = two_year_customers.dropna()
+    two_year_customers = two_year_customers.fillna(0)
+    
+    # Change total charges to float
     two_year_customers.total_charges = two_year_customers.total_charges.astype(float)
     
     return two_year_customers
